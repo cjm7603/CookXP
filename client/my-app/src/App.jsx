19a49504d1e9ae2959a8bd58e1746635929bce90
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2025-03-26 15:59:37 -0400

    Home Page Created

--- client/my-app/src/App.jsx
+++ client/my-app/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import Login  from "./pages/Login";
 import Signup  from "./pages/Signup";
 import Welcome from "./pages/Welcome"; 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserContextProvider, { useUser } from './context/UserContext';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +16,7 @@ function App() {
           <Route path="/" element={<Welcome />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </UserContextProvider>
     </BrowserRouter>

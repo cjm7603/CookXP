eecc9d2c850ff3e82bed3a81964007b8bd8ea3ea
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2025-04-15 14:43:25 -0400

    Friend Attribute Defined

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true},
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, unique: true}
+  password: { type: String, required: true },
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model('User', UserSchema);
